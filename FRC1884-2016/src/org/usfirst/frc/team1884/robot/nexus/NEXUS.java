49956b76c0279a92b7bdaa218e02b8f52c3e382e
Kaito Arai <kaito_arai@asl.org>
2016-02-06 09:54:59 +0000

    Formating

--- FRC1884-2016/src/org/usfirst/frc/team1884/robot/nexus/NEXUS.java
+++ FRC1884-2016/src/org/usfirst/frc/team1884/robot/nexus/NEXUS.java
@@ -1,16 +1,14 @@
 package org.usfirst.frc.team1884.robot.nexus;
 
-import org.usfirst.frc.team1884.robot.sensors.UltrasonicSensor;
-
 import edu.wpi.first.wpilibj.Joystick;
 
 /**
-* NEXUS EXchanges Useful Sensors
-*/
+ * NEXUS EXchanges Useful Sensors
+ */
 public class NEXUS {
-  public static final Joystick JOYSTICK;
+	public static final Joystick JOYSTICK;
 
-  static {
-    JOYSTICK = new Joystick(0);
-  }
+	static {
+		JOYSTICK = new Joystick(0);
+	}
 }
