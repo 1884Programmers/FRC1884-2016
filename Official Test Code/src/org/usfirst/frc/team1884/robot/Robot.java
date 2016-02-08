c2adbc0c5b9e0838a80580211a7d506009e22c38
wxenakis19 <william_xenakis@asl.org>
2016-02-08 19:52:44 +0000

    Adjusted Shooter Code

--- Official Test Code/src/org/usfirst/frc/team1884/robot/Robot.java
+++ Official Test Code/src/org/usfirst/frc/team1884/robot/Robot.java
@@ -1,6 +1,6 @@
 package org.usfirst.frc.team1884.robot;
 
-import org.usfirst.frc.team1884.robot.subsystems.GRIP; 
+import org.usfirst.frc.team1884.robot.subsystems.GRIP;
 import org.usfirst.frc.team1884.robot.subsystems.Proportional;
 import org.usfirst.frc.team1884.robot.subsystems.Spike;
 import org.usfirst.frc.team1884.robot.subsystems.Ultrasonic;
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 
-
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -21,9 +20,9 @@ import edu.wpi.first.wpilibj.Timer;
  */
 public class Robot extends IterativeRobot {
 
-	//private CANTalon intake, shooter;
-	//private Proportional p;
-	//private Joystick joystick;
+	private CANTalon intake, shooter;
+	private Proportional p;
+	private Joystick joystick;
 	private Ultrasonic x;
 
 	/**
@@ -31,12 +30,12 @@ public class Robot extends IterativeRobot {
 	 * used for any initialization code.
 	 */
 	public void robotInit() {
-		//joystick = new Joystick(0);
-//		intake = new CANTalon(5);
-//		shooter = new CANTalon(4);
-//		shooter.setFeedbackDevice(FeedbackDevice.QuadEncoder);
-//		shooter.setControlMode(0);
-		//p = new Proportional(0.1 / 4000.0, -30000.0);
+		joystick = new Joystick(0);
+		intake = new CANTalon(0);
+		shooter = new CANTalon(2);
+		shooter.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		shooter.setControlMode(0);
+		p = new Proportional(-1 / 4000.0, -30000.0);
 		x = Ultrasonic.INSTANCE;
 	}
 
@@ -58,24 +57,26 @@ public class Robot extends IterativeRobot {
 	 * This function is called periodically during autonomous
 	 */
 	public void autonomousPeriodic() {
-		
+
 	}
 
 	/**
 	 * This function is called periodically during operator control
 	 */
 	public void teleopPeriodic() {
-//		if (joystick.getRawButton(1))
-//			Spike.INSTANCE.turnOn();
-//		if (joystick.getRawButton(2))
-//			Spike.INSTANCE.turnOff();
-//		//System.out.println(GRIP.INSTANCE.getCenter());
-//		double i = p.getOutput(shooter.getEncVelocity());
-//		intake.set(-1);
-//		shooter.set(0.8 + i);
-		// System.out.println(shooter.getEncVelocity()+" "+(0.8+i));
-		Ultrasonic.INSTANCE.runAdjustToEightFeet();
+		if (joystick.getRawButton(4))
+			Spike.INSTANCE.turnOn();
+		if (joystick.getRawButton(2))
+			Spike.INSTANCE.turnOff();
+		if (joystick.getRawButton(1)) {
+			intake.set(-1);
+			shooter.set(-1);
+		} else{
+			intake.set(0);
+			shooter.set(0);
 		}
+		Ultrasonic.INSTANCE.runAdjustToEightFeet();
+	}
 
 	/**
 	 * This function is called periodically during test mode
