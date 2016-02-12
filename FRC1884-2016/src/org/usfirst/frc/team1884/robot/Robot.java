0e181e607bb1ec561e2bfdfdbdfcf65f869dcac6
Will V <will_vauclain@asl.org>
2016-02-12 14:10:33 +0000

    Revert "Merge remote-tracking branch '1884Programmers/master' into test_code"
    This reverts commit dba806e63a5ca86cfaba88bc3ba0a747821813d0, reversing
    changes made to fdee35a59cd8cfebda1c29525405206384b98751.

--- FRC1884-2016/src/org/usfirst/frc/team1884/robot/Robot.java
+++ FRC1884-2016/src/org/usfirst/frc/team1884/robot/Robot.java
@@ -1,11 +1,6 @@
+
 package org.usfirst.frc.team1884.robot;
 
-import org.usfirst.frc.team1884.robot.commands.AutonomousHandler;
-import org.usfirst.frc.team1884.robot.subsystems.Elevator;
-import org.usfirst.frc.team1884.robot.subsystems.Hanger;
-import org.usfirst.frc.team1884.robot.subsystems.Shooter;
-import org.usfirst.frc.team1884.robot.subsystems.Subsystem;
-import org.usfirst.frc.team1884.robot.subsystems.WestCoastGearbox;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 /**
@@ -16,38 +11,33 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class Robot extends IterativeRobot {
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
 
-	public void robotInit() {
-		AutonomousHandler.INSTANCE.bar();
-	}
+    }
 
-	/**
-	 * This function is called once at the beginning of autonomous
-	 */
-	public void autonomousInit() {
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
 
-	}
+    }
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
-	public void autonomousPeriodic() {
-
-	}
-
-	/**
-	 * This function is called once at the beginning of operator control
-	 */
-	public void teleopInit() {
-	}
-
-	/**
-	 * This function is called periodically during operator control
-	 */
-	public void teleopPeriodic() {
-	}
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        
+    }
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    
+    }
+    
 }
---
+++ Unsorted Code/West Coast Gearbox Test/src/org/usfirst/frc/team1884/robot/Robot.java
@@ -0,0 +1,37 @@
+
+package org.usfirst.frc.team1884.robot;
+
+import org.usfirst.frc.team1884.robot.subsystems.WestCoastGearbox;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you compute the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+
+	WestCoastGearbox gearbox;
+	Joystick joystick;
+
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+    	joystick = new Joystick(0);
+    	gearbox = WestCoastGearbox.instance;
+    	gearbox.setJoystick(joystick);
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+    	gearbox.teleopPeriodic();
+    }
+}
