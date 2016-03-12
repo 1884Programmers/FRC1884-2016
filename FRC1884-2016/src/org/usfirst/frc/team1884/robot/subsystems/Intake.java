a8dcbfa146fa1b193cf1f49096a365e432454737
Will V <will_vauclain@asl.org>
2016-03-12 16:58:32 -0500

    Cleaned Intake Class

--- FRC1884-2016/src/org/usfirst/frc/team1884/robot/subsystems/Intake.java
+++ FRC1884-2016/src/org/usfirst/frc/team1884/robot/subsystems/Intake.java
@@ -4,18 +4,12 @@ import org.usfirst.frc.team1884.robot.NEXUS;
 import org.usfirst.frc.team1884.robot.commands.IntakeTeleopPeriodic;
 
 import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.CANTalon.FeedbackDevice;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Intake extends Subsystem {
-	private static final int SHOOTER_CHANNEL = 6;
 	private static final int INTERNAL_INTAKE_CHANNEL = 3;
 	private static final int EXTERNAL_INTAKE_CHANNEL = 4;
-
-	private static final double kp = 1.0;
-	private static final int setVelocity = 29000;
 
 	public static final Intake INSTANCE;
 
@@ -26,19 +20,16 @@ public class Intake extends Subsystem {
 	}
 
 	private CANTalon internalIntake, externalIntake;
-	private Proportional p;
 
 	private Intake() {
 		internalIntake = new CANTalon(INTERNAL_INTAKE_CHANNEL);
 		externalIntake = new CANTalon(EXTERNAL_INTAKE_CHANNEL);
 
-		getInternalIntake().enableBrakeMode(true);
-		getInternalIntake().setControlMode(0);
+		internalIntake.enableBrakeMode(true);
+		internalIntake.setControlMode(0);
 
-		getExternalIntake().enableBrakeMode(true);
-		getExternalIntake().setControlMode(0);
-
-		p = new Proportional(kp, setVelocity);
+		externalIntake.enableBrakeMode(true);
+		externalIntake.setControlMode(0);
 		joystick = NEXUS.OPERATORSTICK;
 	}
 
@@ -70,10 +61,6 @@ public class Intake extends Subsystem {
 		externalIntake.set(1);
 	}
 
-	/**
-	 *
-	 *
-	 */
 	public void teleopPeriodic() {
 		if (joystick.getRawAxis(2) > 0.1) {
 			shootLowGoal();
